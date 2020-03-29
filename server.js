d1f581de792e78c5332e090c154f8e04b320e343
Pradeep <pradeeps@techprovint.com>
2020-03-29 17:19:10 +0530

    update influx db after pusher update

--- server.js
+++ server.js
@@ -1,6 +1,25 @@
 var express = require('express');
 var path = require('path');
 var bodyParser = require('body-parser');
+const Influx = require('influx');
+
+const influx = new Influx.InfluxDB({
+    host: '34.73.38.49',
+    database: 'sleep',
+    username: 'smartsleep',
+    password: 'smartsleep123',
+    schema: [
+        {
+            measurement: 'sound_stream',
+            fields: {
+                value: Influx.FieldType.FLOAT
+            },
+            tags: [
+                'device_id'
+            ]
+        }
+    ]
+})
 
 var Pusher = require('pusher');
 
@@ -19,7 +38,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 var heartbeatData = {
-    device: 'smart-sleep-001',
+    device: 'smartsleep-v1',
     unit: 'BPM',
     dataPoints: [
     ]
@@ -29,18 +48,33 @@ app.get('/get-heartbeat', function (req, res) {
     res.send(heartbeatData);
 });
 
-app.get('/add-heartbeat/:heartbeat', function (req, res) {
+app.get('/add-heartbeat/:device/:heartbeat', function (req, res) {
+    var device = req.params.device;
     var heartbeat = parseInt(req.params.heartbeat);
-    if (heartbeat && !isNaN(heartbeat)) {
+
+    if (device && device.length > 0 && heartbeat && !isNaN(heartbeat)) {
         var newDataPoint = {
             heartbeat: heartbeat,
             time: new Date().getTime()
         };
+
+        heartbeatData.device = device;
         heartbeatData.dataPoints.push(newDataPoint);
         pusher.trigger('heartbeat', 'new-heartbeat', {
             dataPoint: newDataPoint
         });
         res.send({ success: true });
+
+        influx.writePoints([
+            {
+                measurement: 'sound_stream',
+                tags: { device_id: device },
+                fields: { 'value': heartbeat }
+            }
+        ]).catch(err => {
+            console.error(`error saving data to influxdb! ${err.stack}`)
+        });
+
     } else {
         res.send({ success: false, errorMessage: 'Invalid Query Paramaters, required - heartbeat & time.' });
     }
